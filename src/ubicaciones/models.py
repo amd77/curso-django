b03abe24174f12f8d821440f699998ebe07eb224
Alberto Morales <noreply@amd77.es>
2022-04-27 09:57:17 +0100

    feat: agregando modelos externos (comillas)

--- src/ubicaciones/models.py
+++ src/ubicaciones/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Edificio(models.Model):
     nombre = models.CharField(max_length=20)
+    habitacion_principal = models.ForeignKey('Habitacion', on_delete=models.SET_NULL, blank=True, null=True, related_name="adf")
+    ususario = models.ForeignKey('auth.User', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return f"Edificio: {self.nombre}"
